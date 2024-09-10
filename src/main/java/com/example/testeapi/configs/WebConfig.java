cf36205caf02fe42c749ca295af0783292ebe393
devlarre <dev.larre@outlook.com>
2024-09-10 16:49:00 -0300

    Created Dockerfile, finalized for deploy

--- src/main/java/com/example/testeapi/configs/WebConfig.java
+++ src/main/java/com/example/testeapi/configs/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5501", "http://127.0.0.1:5500" )
+                        .allowedOrigins("http://127.0.0.1:5501", "http://127.0.0.1:5500", "https://devlarre.github.io/ConsumoBack-end-api/" )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
                         .exposedHeaders("Authorization", "Cache-Control", "Content-Type")
