fa39ccd09edb39945e3f9a1c46fbcca43f6c37cc
devlarre <dev.larre@outlook.com>
2024-09-10 16:49:42 -0300

    Created Dockerfile, finalized for deploy

--- src/main/java/com/example/testeapi/configs/WebConfig.java
+++ src/main/java/com/example/testeapi/configs/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+/* teste*/
 @Configuration
 public class WebConfig {
     @Bean
