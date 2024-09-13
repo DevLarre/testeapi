a34b1bf539475431b5c56d9983fe0955906ca135
devlarre <dev.larre@outlook.com>
2024-09-12 22:13:52 -0300

    Implemented the test for the project, facilitating code maintenance

--- src/main/java/com/example/testeapi/configs/WebConfig.java
+++ src/main/java/com/example/testeapi/configs/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://devlarre.github.io/ConsumoBack-end-api", "http://localhost:3000", "http://127.0.0.1:5501", "http://127.0.0.1:5500" )
+                        .allowedOrigins("https://devlarre.github.io/ConsumoBack-end-api", "https://random-user-backend-saved.netlify.app", "http://127.0.0.1:5501", "http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
                         .exposedHeaders("Authorization", "Cache-Control", "Content-Type")
